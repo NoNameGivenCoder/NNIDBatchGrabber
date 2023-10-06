3ca4377294a168c7c5a1be67cf5e7a2527cb0ec1
NoNameGiven <114644239+NoNameGivenCoder@users.noreply.github.com>
2023-10-06 00:09:27 -0700

    basic commit, most things here

--- index.js
+++ index.js
@@ -1,12 +1,60 @@
-var net = require('net')
-const { serialize, Serializer } = require('v8')
+const fs = require('fs')
+const path = require('path')
+const https = require('https')
 
-var server = net.createServer()
+const axios = require('axios')
 
-server.on('connection', (socket) => {
-    console.log('connection')
-})
+var nnids = JSON.parse(fs.readFileSync(__dirname + "/nnids.json").toString())
 
-server.listen(9000, () => {
-    console.log('Listening!')
-})
+function setUp() {
+    if (nnids.length < 1) {
+        console.log(`Not enough NNID's`)
+
+        return;
+    }
+}
+
+setUp()
+
+async function GetData() {
+    for (let i = 0; i < nnids.length; i++) {
+        //Getting data
+        const nnid = nnids[i];
+        const accountData = (await axios.get(`https://nnidlt.murilo.eu.org/api.php?env=production&user_id=${nnid}`))
+
+        //Setting here for simplicity
+        const path = `/data/${nnid}`
+
+        console.log(`Creating directories at ${path}`)
+
+        fs.mkdirSync(__dirname + path)
+        fs.mkdirSync(__dirname + `${path}/images`)
+
+        fs.writeFileSync(__dirname + `${path}/data.json`, Buffer.from(JSON.stringify(accountData.data, null, 4)))
+
+        for (let i = 0; i < accountData.data.images.image.length; i++) {
+            const image = accountData.data.images.image[i]
+
+            //Getting correct format for all images, since one is TGA
+            const format = image.url.split('.')
+
+            const writer = fs.createWriteStream(__dirname + `${path}/images/${nnid}_${image.type}.${format[4]}`)
+
+            //Having to set it to reject unauthorized, since without that, cdn files wont be able to be downloaded..
+            const imageData = await axios({
+                url: image.url,
+                method: "GET",
+                responseType: "stream",
+                httpsAgent: new https.Agent({
+                    rejectUnauthorized : false
+                })
+            })
+
+            imageData.data.pipe(writer)
+
+            console.log(`Got image for ${nnid}'s ${image.type} expression`)
+        }
+    }
+}
+
+GetData()
